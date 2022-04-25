6132513724f9d631926c213ff1c054f9d64b6a69
medhat <medhatabdelmoniem95@outlook.com>
2022-04-25 13:20:47 +0200

    finished testing

---
+++ src/tests/imageResizerUtilSpec/imageResizerSpec.ts
@@ -0,0 +1,16 @@
+import app from '../..';
+import supertest from 'supertest';
+
+const request = supertest(app);
+
+describe('Testing wrong api calls', ()=>{
+    it('Testing a wrong filename', async () => {
+        const response = await (await request.get('/images?filename=test')).text;
+        expect(response).toEqual('Please write filename or a size');
+      });
+
+      it('Testing with no width', async () => {
+        const response = await (await request.get('/images?filename=palmtunnel')).text;
+        expect(response).toEqual('Please write filename or a size');
+      });
+});
