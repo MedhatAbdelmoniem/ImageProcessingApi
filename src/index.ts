03611404243b9636c0edc0f174c159048bc6f338
medhat <medhatabdelmoniem95@outlook.com>
2022-04-25 08:39:32 +0200

    building the basics

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('Hello and welcome to Image Resizer');
+});
+
+app.listen(port);
+
+export default app;
